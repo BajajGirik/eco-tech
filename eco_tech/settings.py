5254fc1436a38e4891f54a362a7882d89c59f785
Girik Bajaj <bajajgirik2010@gmail.com>
2023-09-07 19:37:42 +0530

    Add .env.example and load env variables from file

--- eco_tech/settings.py
+++ eco_tech/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from dotenv import load_dotenv
+# Load environment variables
+load_dotenv()
+
 from pathlib import Path
 import os
 
