28f94479c8410e1ed66609995486e1a5d4c94af9
Girik Bajaj <bajajgirik2010@gmail.com>
2023-09-11 21:03:56 +0530

    Add mysql configuration

--- eco_tech/settings.py
+++ eco_tech/settings.py
@@ -80,8 +80,10 @@ WSGI_APPLICATION = "eco_tech.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USER"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
     }
 }
 
