3d0cf505c837d1a411a47b7edc7b0ec815514966
Girik Bajaj <bajajgirik2010@gmail.com>
2023-09-15 03:37:14 +0530

    Add error message for failed account creation

--- apps/auth/views.py
+++ apps/auth/views.py
@@ -5,6 +5,7 @@ class RegisterAPIView(generics.CreateAPIView):
     serializer_class = UserSerializer
     response_format = {
         "success_message": "Account created successfully. Please login to continue",
+        "error_message": "Account creation failed",
         "success_data_key": "user",
         "error_data_key": "error",
     }
