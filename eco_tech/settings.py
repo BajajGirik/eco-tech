996a5f9932b7910a5b5c60eaf5c3b79d15fc570b
Girik Bajaj <bajajgirik2010@gmail.com>
2023-09-19 14:52:29 +0530

    Update ALLOWED_HOSTS settings

--- eco_tech/settings.py
+++ eco_tech/settings.py
@@ -31,7 +31,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "lk6al5vc9b.execute-api.ap-south-1.amazonaws.com",
+]
 
 
 # Application definition
