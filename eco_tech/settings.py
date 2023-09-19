1c5aa5423589a74a6beb1d19cbf468497091b514
Girik Bajaj <bajajgirik2010@gmail.com>
2023-09-19 16:46:03 +0530

    Update STATIC_URL based on DEBUG
    If DEBUG is True, static resources/files are picked from from local instead
    of CDN. This is done to reduce unnecessary calls to CDN for resources when
    developing locally

--- eco_tech/settings.py
+++ eco_tech/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "static/" if DEBUG else "https://d37sx5th1ij9bi.cloudfront.net/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
